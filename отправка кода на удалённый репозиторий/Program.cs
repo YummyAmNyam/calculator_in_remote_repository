3f8dec2fef51f37f66aaa21dc7bcec4438f2acd3
YummyAmNyam <dima.andriyanov.6@mail.ru>
2025-03-08 23:51:58 +0300

    Add project files.

---
+++ отправка кода на удалённый репозиторий/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+public class MainClass
+{
+    public static void Main()
+    {
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        string c = Console.ReadLine();
+        if (c == "+") Console.WriteLine(a + b);
+        else if (c == "-") Console.WriteLine(a - b);
+        else if (c == "*") Console.WriteLine(a * b);
+        else if (c == "/" && b != 0) Console.WriteLine((double)a / b);
+        else if (b == 0) Console.WriteLine("На ноль делить нельзя!");
+        else Console.WriteLine("Неверная операция");
+    }
+}
